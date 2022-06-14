d5645f9aad047387ca4c4b66a5ccd1d520c6bb75
lymine1996 <lymine1996@163.com>
2022-06-14 17:00:46 -0500

    Resolve redux error without installation of devtools extension

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -7,8 +7,7 @@ export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    // window.__REDUX_DEVTOOLS_EXTENSION__
-    //   && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
